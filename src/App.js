38d1b27fa441c56df917c27008648a092f03512b
Luisf543678 <luisf543678@gmail.com>
2021-08-13 23:47:26 -0500

    test1

--- src/App.js
+++ src/App.js
@@ -32,27 +32,39 @@ function App() {
       <div className="mx-auto">
         <h2>Drawing App</h2>
         <div className="text-center">
-         <div className="d-grid gap-2 col-6 mx-auto mb-4">
-         <button className="btn btn-success" onClick={handleClick}>Save Drawing</button>
-          <button className="btn btn-danger" onClick={clear}>Clear</button>
-          <button className="btn btn-warning" onClick={undo}>undo</button>
+         <div className="">
+         
+          
+         <div class="d-flex flex-row">
+  <div class="p-2"><button className="btn btn-success tn-lg" onClick={handleClick}>
+    <img src="https://icons-for-free.com/iconfiles/png/512/save+icon-1320167995084087448.png"/>Save Drawing</button></div>
+  <div class="p-2"><button className="btn btn-danger btn-lg" onClick={clear}><img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/delete-1432400-1211078.png"/>Clear</button></div>
+  <div class="p-2"><button className="btn btn-warning btn-lg" onClick={undo}><img src="https://pics.freeicons.io/uploads/icons/png/9739889771543238875-512.png"/>undo</button></div>
+</div>
          </div>
         </div>
       </div>
-      <div className="row justify-content-evenly">
-      <CanvasDraw
+      <div className="row justify-content ">
+     <div className="col">
+     <CanvasDraw
         brushRadius={1}
         brushColor='#130f40'
         catenaryColor='red'
         hideGrid={true}
         style={{ border: '1px solid #000' }}
         ref={firstCanvas}
-        canvasHeight={1000}
-        canvasWidth={1000}
+        canvasHeight={440}
+        canvasWidth={460}
       />
-      <br />
-      <CanvasDraw ref={secondCanvas} hideGrid={true} disabled={true} />
+     </div>
+      
+      <div  className="col"> 
+      <CanvasDraw ref={secondCanvas} canvasHeight={440}
+        canvasWidth={460}  style={{ border: '1px solid #000' }} hideGrid={true} disabled={true} />
       </div>
+      </div>
+      
+
     </div>
   );
 }
